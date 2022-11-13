37c33dc131d991485833a3f55ef784a9686fc96d
Patrycja K <patikuk@onet.pl>
2022-11-13 11:07:16 +0100

    new

--- src/test/java/SIIShop/SIIShopLoginTest.java
+++ src/test/java/SIIShop/SIIShopLoginTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 public class SIIShopLoginTest {
     private WebDriver driver;
     @Test
@@ -17,10 +20,19 @@ public class SIIShopLoginTest {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("start-maximized");
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        chromeOptions.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking", "enable-automation"));
         driver = new ChromeDriver(chromeOptions);
         //driver.manage().window().maximize();
         driver.get(url);
-        System.out.println("Tests passed");
+        //WAIT strategy
+        //1. implicit -> czeka w zależnosci od różnych opóźnień środowisk
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(2));//czekanie na załadowanie webelementów
+
+        //2. explicit ->
+        //3. fluent ->
+        System.out.println("Test passed");
         driver.quit();
 
     }
